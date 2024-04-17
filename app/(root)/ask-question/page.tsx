0acc0336de935e59f8b29e629176f2992404db0f
Sarath <cheekatisarath@gmail.com>
2024-04-17 18:56:03 +0530

    till Question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,14 @@
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+import Question from "@/components/forms/Question";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestion;
+export default Page;
