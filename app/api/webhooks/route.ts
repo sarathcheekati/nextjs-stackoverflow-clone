376e27e21f96ea3783e0133ba536f7c7aff8e535
Sarath <cheekatisarath@gmail.com>
2024-04-21 13:27:46 +0530

    fixed webhook error

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,12 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import {
-  createUser,
-  deleteUser,
-  deleteUser,
-  updateUser,
-} from "@/lib/actions/User.action";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/User.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
