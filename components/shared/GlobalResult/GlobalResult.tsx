ef3876c03a2b3ad7307a5782c3d86b3543f8fd43
Sarath <cheekatisarath@gmail.com>
2024-05-02 12:08:16 +0530

    globalsearch

---
+++ components/shared/GlobalResult/GlobalResult.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import GlobalFilters from "../GlobalFilters/GlobalFilters";
+import { globalSearch } from "@/lib/actions/General.action";
+
+const GlobalResult = () => {
+  const searchParams = useSearchParams();
+  const global = searchParams.get("global");
+  const type = searchParams.get("type");
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState([]);
+
+  const renderLink = (type: string, id: string) => {
+    switch (type) {
+      case "questions":
+        return `/question/${id}`;
+
+      case "answer":
+        return `/question/${id}`;
+
+      case "user":
+        return `/profile/${id}`;
+
+      case "tag":
+        return `/tags/${id}`;
+
+      default:
+        return "/";
+    }
+  };
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      setResult([]);
+      setIsLoading(true);
+      try {
+        const result = await globalSearch({ query: global, type });
+        setResult(JSON.parse(result));
+      } catch (error) {
+        console.log(error);
+        throw error;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (global) {
+      fetchResults();
+    }
+  }, [global, type]);
+
+  return (
+    <div className="dark:bg-dark-400 rounded-xl shadow-sm absolute top-full z-10 mt-3 w-full bg-light-800 py-5">
+      <GlobalFilters />
+      <div className="my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50" />
+      <div className="space-y-5">
+        <p className="text-dark400_light900 paragraph-semibold px-5">
+          Top Match
+        </p>
+        {isLoading ? (
+          <div className="flex-center flex-col px-5">
+            <ReloadIcon className="my-2 h-10 w-10 text-primary-500 animate-spin" />
+            <p className="text-dark200_light800 body-regular">Loading...</p>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {result?.length > 0 ? (
+              result?.map((item: any, index: number) => (
+                <Link
+                  href={renderLink(item.type, item.id)}
+                  key={item.type + item.id + index}
+                  className="flex w-full dark:bg-dark500/50 cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50"
+                >
+                  <Image
+                    src="/assets/icons/tag.svg"
+                    alt="tags"
+                    height={18}
+                    width={18}
+                    className="invert-colors mt-1 object-contain"
+                  />
+                  <div className="flex flex-col">
+                    <p className="body-medium text-dark200_light800 line-clamp-1">
+                      {item.title}
+                    </p>
+                    <p className="text-light400_light500 small-medium mt-1 font-bold capitalize">
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="flex-center px-5 flex-col">
+                <p className="text-dark200_light800 body-regular px-5 py-2.5">
+                  Oops, no results found
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default GlobalResult;
--- components/shared/Search/GlobalSearch.tsx
+++ components/shared/Search/GlobalSearch.tsx
@@ -1,10 +1,66 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import GlobalResult from "../GlobalResult/GlobalResult";
+import router from "next/router";
 
 const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("global");
+
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+  const searchContainerRef = useRef(null);
+  useEffect(() => {
+    const handleOutsideClick = (event: any) => {
+      if (
+        searchContainerRef.current &&
+        //@ts-ignore
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+        setSearch("");
+      }
+    };
+    setIsOpen(false);
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [pathname]);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+        newUrl && router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+
+          newUrl && router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, pathname, router, searchParams, query]);
+
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div
+      className="relative w-full max-w-[600px] max-lg:hidden"
+      ref={searchContainerRef}
+    >
       <div className="background-light800_darkgradient flex relative min-h-[56px] items-center grow gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
@@ -15,11 +71,21 @@ const GlobalSearch = () => {
         />
         <Input
           type="text"
-          placeholder="Search"
-          // value=""
-          className="shadow-none outline-none paragraph-regular no-focus placeholder background-light800_darkgradient border-none"
+          placeholder="Global Search"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+            if (!isOpen) {
+              setIsOpen(true);
+            }
+            if (event.target.value === "" && isOpen) {
+              setIsOpen(false);
+            }
+          }}
+          className="text-dark400_light700 shadow-none outline-none paragraph-regular no-focus placeholder background-light800_darkgradient border-none"
         />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 };
